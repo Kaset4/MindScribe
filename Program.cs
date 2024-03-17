41171c150cf563f6cbbe5a3905f0f9dbf7f9c3cf
Kaset4 <85079510+Kaset4@users.noreply.github.com>
2024-03-17 19:06:22 +0400

    Adding ExceptionHandlerMiddleware

--- Program.cs
+++ Program.cs
@@ -62,6 +62,7 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
+app.UseGlobalExceptionHandler();
 app.UseRouting();
 
 app.UseRoleInitializerMiddleware();
